c1aa0d50f694b664d4829d2edd69b25faa2a524b
Md Rezaul Karim <73929234+bdrkreza@users.noreply.github.com>
2021-03-21 22:31:06 +0600

    Update Cart.js
    update

--- src/Components/DestinationDetails/Cart.js
+++ src/Components/DestinationDetails/Cart.js
@@ -1,68 +1,81 @@
 import React from 'react';
+import "./Cart.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowsAltV, faMapMarkerAlt, faUsers } from '@fortawesome/free-solid-svg-icons'
 const Cart = ({ data, origin, destination }) => {
     return (
         <>
-            <div className="card">
-                <div className="card-body">
-                    <p className="card-text">{origin}</p>
-                    <p className="card-text">To</p>
-                    <p className="card-text">{destination}</p>
+            <div className="mb-5">
+                <div className="card card-icon">
+                    <div className="card-body">
 
+                        <h4 className="card-text">
+                            <FontAwesomeIcon className="mr-2 text-primary" icon={faMapMarkerAlt} size="1x" />
+                            {origin}</h4>
+                        <FontAwesomeIcon className="text-warning" icon={faArrowsAltV} size="2x" />
+                        <h4 className="card-text">
+                            <FontAwesomeIcon className="mr-2 text-primary" icon={faMapMarkerAlt} size="1x" />
+                            {destination}</h4>
+
+                    </div>
                 </div>
-            </div>
 
-            <div className="row">
-                <div className="col-xl-12 col-sm-6 col-12">
-                    <div className="card">
-                        <div className="card-content">
-                            <div className="card-body">
-                                <div className="media d-flex">
-                                    <div className="align-self-center">
-                                        <img classNameName="w-25" src={data.img} alt="" />
-                                        <span classNameName="ml-5 text-danger">{data.vehicle}</span>
-                                    </div>
-                                    <div className="media-body text-right">
-                                        <h3>${data.driverCharges}</h3>
+                <div className="row">
+                    <div className="col-xl-12 col-sm-12 col-md-12">
+                        <div className="card">
+                            <div className="card-content">
+                                <div className="card-body">
+                                    <div className="media d-flex">
+                                        <div className="align-self-center Select-Img">
+                                            <img src={data.img} alt="" />
+                                            <span classNameName="ml-5 text-danger">{data.vehicle}</span>
+                                            <FontAwesomeIcon className="text-warning ml-3" icon={faUsers} size="2x" />
+                                        </div>
+                                        <div className="media-body text-right">
+                                            <h3>${data.driverCharges}</h3>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div className="row">
-                <div className="col-xl-12 col-sm-6 col-12">
-                    <div className="card">
-                        <div className="card-content">
-                            <div className="card-body">
-                                <div className="media d-flex">
-                                    <div className="align-self-center">
-                                        <img classNameName="w-25" src={data.img} alt="" />
-                                        <span classNameName="ml-5 text-danger">{data.vehicle}</span>
-                                    </div>
-                                    <div className="media-body text-right">
-                                        <h3>${data.driverCharges}</h3>
+                <div className="row">
+                    <div className="col-xl-12 col-sm-12 col-md-12">
+                        <div className="card">
+                            <div className="card-content">
+                                <div className="card-body">
+                                    <div className="media d-flex">
+                                        <div className="align-self-center Select-Img">
+                                            <img src={data.img} alt="" />
+                                            <span classNameName="ml-5 text-danger">{data.vehicle}</span>
+                                            <FontAwesomeIcon className="text-warning ml-3" icon={faUsers} size="2x" />
+                                        </div>
+                                        <div className="media-body text-right">
+                                            <h3>${data.driverCharges}</h3>
 
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div className="row">
-                <div className="col-xl-12 col-sm-6 col-12">
-                    <div className="card">
-                        <div className="card-content">
-                            <div className="card-body">
-                                <div className="media d-flex">
-                                    <div className="align-self-center">
-                                        <img classNameName="w-25" src={data.img} alt="" />
-                                        <span classNameName="ml-5 text-danger">{data.vehicle}</span>
-                                    </div>
-                                    <div className="media-body text-right">
-                                        <h3>${data.driverCharges}</h3>
+                <div className="row">
+                    <div className="col-xl-12 col-sm-12 col-md-12">
+                        <div className="card">
+                            <div className="card-content">
+                                <div className="card-body">
+                                    <div className="media d-flex">
+                                        <div className="align-self-center Select-Img">
+                                            <img src={data.img} alt="" />
+                                            <span classNameName="ml-5 text-danger">{data.vehicle}</span>
+                                            <FontAwesomeIcon className="text-warning ml-3" icon={faUsers} size="2x" />
+                                        </div>
+                                        <div className="media-body text-right">
+                                            <h3>${data.driverCharges}</h3>
 
+                                        </div>
                                     </div>
                                 </div>
                             </div>
