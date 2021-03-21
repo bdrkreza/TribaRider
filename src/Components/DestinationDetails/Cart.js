e62e0758856d3bdc098c1ac7df6599214822a95a
Md Rezaul Karim <73929234+bdrkreza@users.noreply.github.com>
2021-03-21 23:49:42 +0600

    Update Cart.js
    update

--- src/Components/DestinationDetails/Cart.js
+++ src/Components/DestinationDetails/Cart.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import "./Cart.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowsAltV, faMapMarkerAlt, faUsers } from '@fortawesome/free-solid-svg-icons'
+import { faArrowsAltV, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons'
+import Rider from './Rider';
 const Cart = ({ data, origin, destination }) => {
+    const [riderUser, setriderUser] = useState([])
+    useEffect(() => {
+        setriderUser(data.rider)
+    }, [])
+
     return (
         <>
             <div className="mb-5">
                 <div className="card card-icon">
                     <div className="card-body">
-
                         <h4 className="card-text">
                             <FontAwesomeIcon className="mr-2 text-primary" icon={faMapMarkerAlt} size="1x" />
                             {origin}</h4>
@@ -20,69 +25,10 @@ const Cart = ({ data, origin, destination }) => {
                     </div>
                 </div>
 
-                <div className="row">
-                    <div className="col-xl-12 col-sm-12 col-md-12">
-                        <div className="card">
-                            <div className="card-content">
-                                <div className="card-body">
-                                    <div className="media d-flex">
-                                        <div className="align-self-center Select-Img">
-                                            <img src={data.img} alt="" />
-                                            <span classNameName="ml-5 text-danger">{data.vehicle}</span>
-                                            <FontAwesomeIcon className="text-warning ml-3" icon={faUsers} size="2x" />
-                                        </div>
-                                        <div className="media-body text-right">
-                                            <h3>${data.driverCharges}</h3>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-xl-12 col-sm-12 col-md-12">
-                        <div className="card">
-                            <div className="card-content">
-                                <div className="card-body">
-                                    <div className="media d-flex">
-                                        <div className="align-self-center Select-Img">
-                                            <img src={data.img} alt="" />
-                                            <span classNameName="ml-5 text-danger">{data.vehicle}</span>
-                                            <FontAwesomeIcon className="text-warning ml-3" icon={faUsers} size="2x" />
-                                        </div>
-                                        <div className="media-body text-right">
-                                            <h3>${data.driverCharges}</h3>
-
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-xl-12 col-sm-12 col-md-12">
-                        <div className="card">
-                            <div className="card-content">
-                                <div className="card-body">
-                                    <div className="media d-flex">
-                                        <div className="align-self-center Select-Img">
-                                            <img src={data.img} alt="" />
-                                            <span classNameName="ml-5 text-danger">{data.vehicle}</span>
-                                            <FontAwesomeIcon className="text-warning ml-3" icon={faUsers} size="2x" />
-                                        </div>
-                                        <div className="media-body text-right">
-                                            <h3>${data.driverCharges}</h3>
-
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
             </div>
+            {
+                riderUser.map((data) => <Rider data={data} />)
+            }
         </>
 
     );
